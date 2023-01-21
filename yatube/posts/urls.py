01a1a91233f0d0bfb3b4ef89cc2ea3840eb7decf
Александр Мананников <alexman2505@gmail.com>
2023-01-21 21:06:16 +0300

    version 1. test success

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,12 @@
 from django.urls import path
 from . import views
 
+# Эта строчка обязательна.
+# Без неё namespace работать не будет:
+app_name = 'posts'
+
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts)
+    path('', views.index, name = 'index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from django.http import HttpResponse
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
+
+# View-функция для страницы сообщества:
 def group_posts(request, slug):
-    return HttpResponse('Список групповых постов')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
+    title = 'Записи сообщества'
+    context = {
+        'group': group,
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
